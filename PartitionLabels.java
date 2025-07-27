a79e95ccaa66afd2a0b237a16b729205c3cf81df
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-07-27 18:35:57 -0500

    Create PartitionLabels.java

---
+++ PartitionLabels.java
@@ -0,0 +1,22 @@
+// Time Complexity- O(n)
+// Space complexity -O(1)
+// solved on leetcode - yes
+// faced any issues - No
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        for(int i=0;i<s.length();i++){
+            last[s.charAt(i)-'a']=i;
+        }
+        List<Integer> result = new ArrayList<>();
+        int start=0,end=0;
+        for(int i=0;i<s.length();i++){
+            end = Math.max(end,last[s.charAt(i)-'a']);
+            if(i==end){
+                result.add(end-start+1);
+                start=i+1;
+            }
+        }
+        return result;
+    }
+}
